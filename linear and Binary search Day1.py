b7025a14a38305488b676d6bfe04da8f67a9f7f3
Mopoori <158029122+Mopoori@users.noreply.github.com>
2025-08-04 23:33:27 +0530

    Add files via upload

---
+++ linear and Binary search Day1.py
@@ -0,0 +1,45 @@
+# Linear search
+def linearSearch(arr, targetVal):
+  for i in range(len(arr)):
+    if arr[i] == targetVal:
+      return i
+  return -1
+
+mylist = [3, 7, 2, 9, 5, 1, 8, 4, 6]
+x = 4
+
+result = linearSearch(mylist, x)
+
+if result != -1:
+  print("Found at index", result)
+else:
+  print("Not found")
+
+
+# Binary search
+def binarySearch(arr, targetVal):
+  left = 0
+  right = len(arr) - 1
+
+  while left <= right:
+    mid = (left + right) // 2
+
+    if arr[mid] == targetVal:
+      return mid
+
+    if arr[mid] < targetVal:
+      left = mid + 1
+    else:
+      right = mid - 1
+
+  return -1
+
+mylist = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+x = 11
+
+result = binarySearch(mylist, x)
+
+if result != -1:
+  print("Found at index", result)
+else:
+  print("Not found")
